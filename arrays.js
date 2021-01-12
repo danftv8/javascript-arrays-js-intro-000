e88894234f3bd3184354e6c1ff135cf6918280b7
Daniel Mullaney <danftv@gmail.com>
2021-01-12 00:54:09 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,6 +7,9 @@ function destructivelyAddElementToBeginningOfArray (array, element) {
   array.unshift(element);
   return array
 }
+function addElementToEndOfArray (array, element) {
+  return(...array, element);
+}
 function destructivelyAddElementToEndOfArray (array, element) {
   array.push(element);
   return array
