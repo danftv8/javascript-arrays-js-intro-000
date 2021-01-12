d64952abbf9b4c23a455436beabc321dd94f777b
Daniel Mullaney <danftv@gmail.com>
2021-01-12 00:43:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
-function addElementToBeginingOfArray (array, element) {
+function addElementToBeginningOfArray (array, element) {
   return [element, ...array];
 }
